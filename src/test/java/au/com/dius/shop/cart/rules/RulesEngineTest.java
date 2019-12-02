5803819084dcea8d5e13c5729f1b1bc5c3cd5f44
Krishna Singampalli <Krishna.Singampalli@afl.com.au>
2019-12-02 20:39:31 +1100

    Added validation for rules

--- src/test/java/au/com/dius/shop/cart/rules/RulesEngineTest.java
+++ src/test/java/au/com/dius/shop/cart/rules/RulesEngineTest.java
@@ -3,13 +3,23 @@ package au.com.dius.shop.cart.rules;
 import org.junit.Assert;
 import org.junit.Test;
 
-import au.com.dius.shop.cart.Engine;
+import au.com.dius.shop.cart.engine.RulesEngine;
 
 public class RulesEngineTest {
 
     @Test
     public void load_rules_test() {
-        RulesEngine engine = new RulesEngine();
+        RulesEngine engine = RulesEngine.getEngine();
         Assert.assertEquals(3, engine.getExpressions().size());
+        Assert.assertEquals("atv size = 3, discount atv", engine.getExpressions().get(0).toString());
+    }
+
+    @Test
+    public void validate_rules_test() {
+        RulesEngine engine = RulesEngine.getEngine();
+        Assert.assertTrue( engine.validateExpressions("atv size = 3, discount atv"));
+        Assert.assertTrue( engine.validateExpressions("ipd size > 4, newprice 499.99"));
+        Assert.assertTrue( engine.validateExpressions("mbp size = 1, free vga"));
+
     }
 }
